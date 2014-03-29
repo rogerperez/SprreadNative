6f07c80fff70597891cb55f4647dae77b0d7d412
rogerperez <rogerperez@u.boisestate.edu>
2014-03-28 22:38:07 -0600

    Initial Commit

---
+++ Sprread/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sprread
+//
+//  Created by Roger Perez on 3/28/14.
+//  Copyright (c) 2014 Roger Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Sprread/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sprread
+//
+//  Created by Roger Perez on 3/28/14.
+//  Copyright (c) 2014 Roger Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
