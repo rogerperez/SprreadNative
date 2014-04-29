3c07e8c0ca57499eb775ec1432788714614beff8
rogerperez <rogerperez@u.boisestate.edu>
2014-04-29 10:34:49 -0600

    Added ability for AFNetworking to read JSON file from within the WebViewController

--- Sprread/WebViewController.m
+++ Sprread/WebViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "WebViewController.h"
+#import "FeedController.h"
+#import "AFNetworking.h"
 
 @interface WebViewController() <UIWebViewDelegate>
 
@@ -54,37 +56,18 @@
 
 #pragma mark - UIWebViewDelegate
 
-- (void)webViewDidStartLoad:(UIWebView *)webView
-{
-//    NSURLRequest *currentRequest = webView.request;
-//    NSURL *currentUrl = currentRequest.URL;
-//    NSString *url = currentUrl.absoluteString;
-//    NSLog(@"Hey %@", url);
-    
-    
-}
-
 - (void)webViewDidFinishLoad:(UIWebView *)webView
 {
     // finished loading, hide the activity indicator in the status bar
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
     self.spinner.hidden = YES;
-    
-//    NSURLRequest *currentRequest = webView.request;
-//    NSURL *currentUrl = currentRequest.URL;
-//    NSString *url = currentUrl.absoluteString;
-//    NSLog(@"Hey %@", url);
 
 }
-
-int i = 1;
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
 {
     NSURL *currentUrl = request.URL;
     NSString *url = currentUrl.absoluteString;
-//    NSLog(@"Hey %i = %@", i, url);
-//    i++;
     
     /****
      *  facebook
@@ -93,9 +76,46 @@ int i = 1;
     NSRange rangeOfSubstring = [url rangeOfString:triggerUrlQueryString];
     if(rangeOfSubstring.location != NSNotFound){
         [self dismissViewControllerAnimated:YES completion:nil];
+
+        // present new view controller
+        [self switchToFeedController];
     }
     return YES;
 
+}
+
+- (void)switchToFeedController
+{
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    FeedController *viewController = (FeedController *)[storyboard instantiateViewControllerWithIdentifier:@"feedController"];
+    [self presentViewController:viewController animated:YES completion:nil];
+}
+
+- (void) loadFeedwithController
+
+{
+    NSURL *resourceURLString = [[NSBundle mainBundle] resourceURL];
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@feedPreview.json", resourceURLString]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    operation.responseSerializer = [AFJSONResponseSerializer serializer];
+
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject){
+        NSDictionary *data = responseObject;
+        NSLog(@"%@", data);
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error Retrieving Feed"
+                                                            message:[error localizedDescription]
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"Ok"
+                                                  otherButtonTitles:nil];
+        [alertView show];
+    }];
+    
+    [operation start];
+    
 }
 
 @end
